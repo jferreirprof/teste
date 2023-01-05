dee34d829560e556e3b474b0f238b2ab90a2bc8a
jferreirprof <58809904+jferreirprof@users.noreply.github.com>
2023-01-05 15:23:17 +0000

    Atualização main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showIcon(IconNames.Ghost)
+})
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
     music.playMelody("C5 B A G F E D C ", 120)
+    soma += 1
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showIcon(IconNames.LeftTriangle)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(1000)
+    basic.showNumber(soma)
+    led.plot(1, 16)
 })
-basic.forever(function () {
-	
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showIcon(IconNames.Snake)
 })
+let soma = 0
+soma = 0
