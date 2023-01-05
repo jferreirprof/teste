13a7ee1e480829bd9e43f9013982c156590e6303
jferreirprof <58809904+jferreirprof@users.noreply.github.com>
2023-01-05 14:22:39 +0000

    Atualização main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
+    music.playMelody("C5 B A G F E D C ", 120)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(1000)
