6c304540ff8f497733982a6c756838b69a9cfcb6
jferreirprof <58809904+jferreirprof@users.noreply.github.com>
2023-01-05 14:16:11 +0000

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(1000)
+})
+basic.forever(function () {
+	
+})
